c898b747e186d1063d83de0ba901e564efaa616a
FardadA <imfardadarab@gmail.com>
2025-07-25 10:59:48 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+PORT=${PORT:-10000}
+
 ss-server \
-  -s 0.0.0.0 -p 10000 \
+  -s 0.0.0.0 -p $PORT \
   -k fardad1386 -m aes-256-gcm \
   --plugin v2ray-plugin \
   --plugin-opts "server;path=/ss;host=${RENDER_EXTERNAL_HOST};loglevel=none"
